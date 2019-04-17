d3769c5f35121171c5d2064c120cf11eebaa1445
Екатерина <liberty_12@mail.ru>
2019-04-17 16:29:17 +0300

    delete comments

--- source/js/script.js
+++ source/js/script.js
@@ -14,13 +14,3 @@ navButton.addEventListener("click", function(evt) {
     nav.classList.remove("navigation--opened");
   }
 });
-
-// navToggle.addEventListener('click', function() {
-//   if (navMain.classList.contains('main-nav--closed')) {
-//     navMain.classList.remove('main-nav--closed');
-//     navMain.classList.add('main-nav--opened');
-//   } else {
-//     navMain.classList.add('main-nav--closed');
-//     navMain.classList.remove('main-nav--opened');
-//   }
-// });
