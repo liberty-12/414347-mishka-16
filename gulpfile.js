d8f776d03b123602db888bc4f8b474fa6c857646
Екатерина <liberty_12@mail.ru>
2019-05-05 11:52:54 +0300

    fix task for copying fonts

--- gulpfile.js
+++ gulpfile.js
@@ -77,7 +77,7 @@ gulp.task("js", function() {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**"
   ], {
